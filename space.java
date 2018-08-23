512bc5e1ca05f4805ae3ac0c1015323e1833c6ab
kavyakonduri <41731461+kavyakonduri@users.noreply.github.com>
2018-08-23 10:57:29 +0530

    Create space.java

---
+++ space.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+        int count=0,i=0;
+        String sc=sc.nextLine();
+        // arr[]=s.split("\t");
+        int k=s.length();
+        k=k-1;
+           //System.out.println(k);
+        for(i=0;i<k;i++)
+        {
+         if(s.charAt(i)=='')
+         count++;
+        }
+      System.out.println(""+count);
+	}
+}
